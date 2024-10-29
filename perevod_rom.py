0776d5ab530187569a346f4fc58eba62373672b0
Andrew <sasbomb59@gmail.com>
2024-10-30 00:31:29 +0500

    Финальная версия. Добавлен перевод в римские

--- perevod_rom.py
+++ perevod_rom.py
@@ -15,4 +15,26 @@ def sys(num, sys1, sys2):
 	    return sys(num // sys2, 10, sys2) + a[num % sys2]
 
 
-print(sys(input(), input(), input()))
+def rom(num):
+	num = int(num)
+
+	e = ['', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX']
+	d = ['', 'X', 'XX', 'XXX', 'XL', 'L', 'LX', 'LXX', 'LXXX', 'XC']
+	s = ['', 'C', 'CC', 'CCC', 'CD', 'D', 'DC', 'DCC', 'DCCC', 'CM']
+	t = ['', 'M', 'MM', 'MMM', 'MMMM']
+	
+	rt, num = t[num // 1000], num % 1000
+	rs, num = s[num // 100], num % 100
+	rd, num = d[num // 10], num % 10
+	re = e[num]
+
+	return rt + rs + rd + re
+
+def result(num, sys1, sys2):
+	if sys2 == 'r':
+		return rom(sys(num, sys1, 10))
+	else:
+		return sys(num, sys1, sys2)
+
+print('Перевод чисел, больших 4999 в римские не поддерживается. Отрицательные системы счисления тоже. Поддерживаются только заглавные и строчные латинские буквы, и цифры. Отсутвует проверка корректности ввода ')
+print(result(input('Число '), input('Его система счисления '), input('Итоговая система счисления. r для римских цифр ')))
